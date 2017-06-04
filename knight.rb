12da6b4a5691a12bf462ad38022e13af84750a5c
synion <lach.szymon86@gmail.com>
2017-06-04 14:06:09 +0200

    Knight move

---
+++ knight.rb
@@ -0,0 +1,67 @@
+class Knight
+  VECTORS = [
+              [2, -1], [2, 1], [-2, -1], [-2, 1],
+              [1, -2], [1, 2], [-1, -2], [-1, 2]
+            ]
+  attr_accessor :coordinates_x, :coordinates_y
+
+  def initialize(coordinates)
+    @coordinates_x = coordinates[0]
+    @coordinates_y = coordinates[1]
+  end
+
+  def cord_x
+    Cord.new.cord_x.index(@coordinates_x)
+  end
+
+  def cord_y
+    Cord.new.cord_y.index(@coordinates_y.to_s)
+  end
+
+
+  def view
+    convert_position.join(' ')
+  end
+
+  def to_s
+    "#{view}"
+  end
+
+
+private
+
+
+  def posible
+    VECTORS.map{|x,y| [x + self.cord_x, y + self.cord_y]  }
+  end
+
+  def real_posible
+    posible.select {|x,y| (1..8).include?(x) && (1..8).include?(y) }
+  end
+
+  def convert_position
+    real_posible.map {|x,y| "#{Cord.new.cord_x[x]}#{Cord.new.cord_y[y]}"}.sort
+  end
+
+  class Cord
+    CORD_X = "_abcdefgh"
+    CORD_Y = "_12345678"
+    attr_writer :cord_x, :cord_y
+
+    def inititialize(cord_x,cord_y)
+      @cord_x = cord_x
+      @cord_y = cord_y
+    end
+
+    def cord_x
+      self.cord_x = CORD_X
+    end
+
+    def cord_y
+      self.cord_y = CORD_Y
+    end
+
+  end
+end
+
+puts Knight.new("g2")
